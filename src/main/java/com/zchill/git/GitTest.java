0af2bc0c47ceb9086dad4161301f8c840b747bd8
Zchill <suplilcc@163.com>
2021-04-26 19:51:10 +0800

    first commit

---
+++ src/main/java/com/zchill/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.zchill.git;
+
+/**
+ * @author zchill
+ *
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
